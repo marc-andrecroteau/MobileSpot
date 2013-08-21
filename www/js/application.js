46b9cdd487080ca761f72584fe1f89fdfcaa229c
Kaven Gagne <kaven.gagne@gmail.com>
2013-08-21 17:43:44 -0400

    MAC menu

--- www/js/application.js
+++ www/js/application.js
@@ -1,7 +1,21 @@
 
 
 jQuery(document).ready(function () {
-    jQuery.getScript("js/app/init.js", function () {
-        jQuery.getScript("js/app/login.js");
+    jQuery.get({
+        url: "js/app/init.js",
+        dataType: "script",
+        success: otherScripts
     });
+
+    var otherScripts = function() {
+        jQuery.get({
+            url: "js/app/login.js",
+            dataType: "script"
+        });
+        /*
+        jQuery.getScript("js/app/init.js", function () {
+            jQuery.getScript("js/app/login.js");
+        });
+        */
+    }
 });
