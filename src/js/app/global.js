47263506bc886794303719086f8d4f1df36bec71
Kaven Gagne <kaven.gagne@gmail.com>
2013-09-10 23:11:41 -0400

    added intel debugger

--- src/js/app/global.js
+++ src/js/app/global.js
@@ -1,5 +1,19 @@
 
 
+window.onerror = function(msg, url, line) {
+    // You can view the information in an alert to see things working
+    // like so:
+    alert("Error: " + msg + "\nurl: " + url + "\nline #: " + line);
+
+    // TODO: Report this error via ajax so you can keep track
+    //       of what pages have JS issues
+
+    var suppressErrorAlert = true;
+    // If you return true, then error alerts (like in older versions of
+    // Internet Explorer) will be suppressed.
+    return suppressErrorAlert;
+};
+
 //-----------------------------------------------
 // Global Properties and Overrides
 //-----------------------------------------------
@@ -79,5 +93,3 @@ $.ui.ready(function () {
     });
 
 });
-
-
